9a441e9f98f6c6a82f7709b3d23a8537d90ef107
SzmiSmi <97198979+SzmiSmi@users.noreply.github.com>
2024-03-24 02:21:11 -0400

    Fishes now fly out on beat

--- TipseaDipsea/Assets/SpawnFish.cs
+++ TipseaDipsea/Assets/SpawnFish.cs
@@ -6,15 +6,40 @@ public class SpawnFish : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject fishPrefab;
+    public GameObject target;
+    private Vector3 fishDirection;
+    private Quaternion lookRotation;
+
+    private float timer = 0.0f;
+    private float spawnRate;
+    public float bpm = 105.0f;
     
     void Start()
     {
-        Instantiate(fishPrefab, transform.position, Quaternion.identity);
+        spawnRate = 4*60/bpm;
+        fishDirection = target.transform.position - transform.position;
+        lookRotation = Quaternion.LookRotation(fishDirection);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(timer<spawnRate)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            GameObject fish = Instantiate(fishPrefab, transform.position, lookRotation);
+            Swim swimControl = fish.GetComponent<Swim>();
+            if(swimControl!= null) 
+            {
+                swimControl.SetTarget(target);
+                swimControl.SetBpm(bpm);
+            }
+            timer = 0;
+        }
         
     }
 }
--- TipseaDipsea/Assets/Swim.cs
+++ TipseaDipsea/Assets/Swim.cs
@@ -4,21 +4,49 @@ using UnityEngine;
 
 public class Swim : MonoBehaviour
 {
-    public float speed = 5f;
+    private float speed;    //based on time required to traverse 50 units.
     private Vector3 moveDirection;
-    public Transform vrCamera;
+    private GameObject target;
+
+    private float bpm = 120.0f;
+
+
+    private float deadZone = -25f;
+
+    public void SetTarget(GameObject newTarget)
+    {
+        target = newTarget;
+    }
+    public void SetBpm(float newBpm)
+    {
+        bpm = newBpm;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        moveDirection = (vrCamera.position- transform.position).normalized;
+        if(target!= null)
+        {
+            moveDirection = (target.transform.position - transform.position).normalized;
+        }
+        else{
+            moveDirection = new Vector3(0.0f,1.0f,0.0f);
+        }
         GetComponent<Rigidbody>().useGravity = false;
+        speed = 50*bpm/240;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += moveDirection * speed * Time.deltaTime;
-
+        if(target!= null)
+        {
+            transform.position += moveDirection * speed * Time.deltaTime;
+        }
+        
+        if(transform.position.z<deadZone){
+            Destroy(gameObject);
+        }
     }
 }
